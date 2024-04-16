3689c60000ddf4d63cfb8c86f670aa9e23c807cf
ashishmisal23 <ashishmisal23@gmail.com>
2024-04-17 04:23:36 +0530

    LOGIN & SIGNUP ENDPOINTS CREATED

---
+++ server/db/connectDb.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const connectDb = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_DB_URL);
+        console.log("Connected to MongoDB")
+
+    }
+    catch (error) {
+        console.log("Error Connecting to MongoDB", error.message);
+
+    }
+
+
+};
+
+export default connectDb;
--- server/server.js
+++ server/server.js
@@ -1,19 +1,21 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+import authRoutes from "./routes/authRoutes.js";
+import connectDb from "./db/connectDB.js";
 
 const app = express()
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 
-app.get("/", (req, res) => {
-    res.send("Hello")
+app.use(express.json());    //middleware
 
-})
+app.use("/api/auth", authRoutes);
 
-app.get("/api/auth/signup", (req, res) => {
-    res.send("Signup")
 
-})
 
-app.listen(PORT, () => console.log(`Server is Running on port ${PORT}`));
+app.listen(PORT, async () => {
+    console.log(`Server is Running on port ${PORT}`)
+    connectDb();
+});
